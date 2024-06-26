e1bb633c40b411a5bd4d90f1b5788499da1aee1f
Surachet Intamongkol <diymos78@gmail.com>
2024-06-26 12:57:24 +0700

    Add Nav

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import Header from "./components/Header";
+import Nav from "./components/Nav";
 export default function Home() {
   return (
     <main >
       <Header />
+      <Nav />
     </main>
   );
 }
