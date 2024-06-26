878ab9fa5bc5597b48e06880718644d088171e9b
Surachet Intamongkol <diymos78@gmail.com>
2024-06-26 14:33:26 +0700

    Add Img

--- src/app/about/team/page.jsx
+++ src/app/about/team/page.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import Link from 'next/link'
-import Header from '../components/Header'
-import Nav from '../components/Nav'
+import nextImg from '../../../../public/next.svg'
+import vercelImg from '../../../../public/vercel.svg'
+import Image from 'next/image'
+import Header from "../../components/Header";
+import Nav from "../../components/Nav";
+
 function page() {
   return (
     <div>
-        <h3>Our Team</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis cumque ab quod accusamus quidem. Suscipit voluptate voluptatum dignissimos laudantium nam?</p>
-        <Link href="/about" className='inline-block bg-purple-500 text-white py-2 px-4 rounded'>Back</Link>
+      <Header />
+      <Nav />
+      <h3>Our Team</h3>
+      <Image src={nextImg} width={300} height={100} alt='next image' />
+      <Image src={vercelImg} width={300} height={100} alt='vercel image' />
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis cumque ab quod accusamus quidem. Suscipit voluptate voluptatum dignissimos laudantium nam?</p>
+      <Link href="/about" className='inline-block bg-purple-500 text-white py-2 px-4 rounded'>Back</Link>
     </div>
   )
 }
