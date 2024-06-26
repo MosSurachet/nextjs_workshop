548f937b9b70a04f4cad4b39a3fac00630477c1b
Surachet Intamongkol <diymos78@gmail.com>
2024-06-26 14:50:30 +0700

    Data fetching

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
-import Header from '../components/Header'
-import Nav from '../components/Nav'
 
-function BlogPage() {
+async function fetchBlogData(){
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await response.json();
+  return data;
+}
+
+async function BlogPage() {
+
+  const blogData = await fetchBlogData();
+  console.log(blogData);
+
   return (
     <div>
       <h3>Blog Page</h3>
       <p>this is content of Blog page</p>
+      <hr />
+      {!blogData ? (
+        <div>Loading....</div>
+      ) : (
+        <>
+          {blogData.map((post) => (
+            <div key={post.id}>
+                <h3 className='font-bold my-3'>{post.title}</h3>
+                <p>{post.body}</p>
+            </div>
+          ))}
+        </>
+      )}
     </div>
   )
 }
